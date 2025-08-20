221da1ff1d92aef4bae817eec6b4c56bf2d5e8b2
pengzhifeng <pengzhifeng@bytedance.com>
2025-08-20 23:31:25 +0800

    feat(TBaseTools): Version 2 Test Example

--- src/main/java/org/example/RocksDBConnector.java
+++ src/main/java/org/example/RocksDBConnector.java
@@ -1,65 +1,57 @@
 package org.example;
 
-import com.google.common.cache.Cache;
-import com.google.common.cache.CacheBuilder;
-import org.jboss.netty.handler.codec.http.HttpMethod;
-import org.restexpress.Request;
-import org.restexpress.Response;
-import org.restexpress.RestExpress;
-import org.terarkdb.*;
-import org.terarkdb.HdfsEnv;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-/*
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-*/
-
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
 import java.util.Random;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.terarkdb.ColumnFamilyHandle;
+import org.terarkdb.FlushOptions;
+import org.terarkdb.Options;
+import org.terarkdb.RocksDB;
+import org.terarkdb.RocksDBException;
+import org.terarkdb.TBaseTableIterator;
+import org.terarkdb.TBaseTools;
+
 /**
- * 
- The given code block contains a simple counter using rocksdb's merge operator
- 
- the Counters Fails for the following Cases
- 
- $ ab -r -k -n 10000 -c 1000 http://localhost:9009/increment
-
- $ curl http://localhost:9009/get                                                                                                                                                           
-
- Current RocksDB Count is 137   (Expected value is 10000)
- Current AtomicLong Count is 10000
-
- $ curl http://localhost:9009/reset
-
- Current RocksDB Count is 0
- Current AtomicLong Count is 0
-
- $ ab -r -k -n 10000 -c 1000 http://localhost:9009/batchIncrement
-
- $ curl http://localhost:9009/get
-
- Current RocksDB Count is 16   (Expected value is 10000)
- Current AtomicLong Count is 10000
- 
+ *
+ * The given code block contains a simple counter using rocksdb's merge operator
+ *
+ * the Counters Fails for the following Cases
+ *
+ * $ ab -r -k -n 10000 -c 1000 http://localhost:9009/increment
+ *
+ * $ curl http://localhost:9009/get  *
+ * Current RocksDB Count is 137 (Expected value is 10000) Current AtomicLong
+ * Count is 10000
+ *
+ * $ curl http://localhost:9009/reset
+ *
+ * Current RocksDB Count is 0 Current AtomicLong Count is 0
+ *
+ * $ ab -r -k -n 10000 -c 1000 http://localhost:9009/batchIncrement
+ *
+ * $ curl http://localhost:9009/get
+ *
+ * Current RocksDB Count is 16 (Expected value is 10000) Current AtomicLong
+ * Count is 10000
+ *
  */
 public class RocksDBConnector {
 
     private static final Logger LOG = LoggerFactory.getLogger(RocksDBConnector.class);
 
-    private  static  RocksDB db;
+    private static RocksDB db;
 
     private static ColumnFamilyHandle cfHandle;
 
     private static long startTime;
     private static long endTime;
 
-    private  RocksDBConnector(){}
+    private RocksDBConnector() {
+    }
 
     private final static RocksDBConnector instance = new RocksDBConnector();
 
@@ -71,7 +63,7 @@ public class RocksDBConnector {
         } catch (IOException e) {
             System.out.print(e);
             LOG.info(e.getMessage());
-        }catch(InterruptedException e){
+        } catch (InterruptedException e) {
             System.out.print(e);
             LOG.info(e.getMessage());
         } catch (RocksDBException e) {
@@ -80,32 +72,33 @@ public class RocksDBConnector {
         }
     }
 
-    public static String getRandomString(int length){
-        String str="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-        Random random=new Random();
-        StringBuffer sb=new StringBuffer();
-        for(int i=0;i<length;i++){
-        int number=random.nextInt(62);
-        sb.append(str.charAt(number));
+    public static String getRandomString(int length) {
+        String str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        Random random = new Random();
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < length; i++) {
+            int number = random.nextInt(62);
+            sb.append(str.charAt(number));
         }
         return sb.toString();
     }
 
-    private static void db_load()throws RocksDBException, InterruptedException{
-        for(int count = 0; count < 8; ++count) {
-            if(count == 4){
-                Thread.sleep(2000);
+    private static void db_load() throws RocksDBException, InterruptedException {
+        for (int count = 0; count < 8; ++count) {
+            if (count == 4) {
                 startTime = System.currentTimeMillis() / 1000L;
                 System.out.println("start load time: " + String.valueOf(startTime));
+                Thread.sleep(2000);
             }
-            for(int i =0 ;i< 8;i++){
+            for (int i = 0; i < 8; i++) {
                 String key = getRandomString(8);
                 String value = getRandomString(i);
                 //for(int j = 0;j < 1000;j++){ 
-                db.put(key.getBytes(),value.getBytes());
+                db.put(key.getBytes(), value.getBytes());
                 //}
                 //System.out.println("finished load " + String.valueOf(i));
             }
+            Thread.sleep(1000);
             db.flush(new FlushOptions().setWaitForFlush(true));
             System.out.println("finished load sst " + String.valueOf(count));
         }
@@ -126,11 +119,12 @@ public class RocksDBConnector {
         System.out.println(separator);
 
         cfHandle = db.getDefaultColumnFamily();
+        String dbPath = "/tmp/testdata";
 
         // Test 1: getTablesByCreationTime
         System.out.printf("%n[TEST] Running: getTablesByCreationTime%n");
         System.out.printf("[INFO] Searching for tables between %d and %d%n", startTime, endTime);
-        List<byte[]> tableNames = TBaseTools.getTablesByCreationTime(cfHandle, startTime, endTime);
+        List<byte[]> tableNames = TBaseTools.getTablesByCreationTime(dbPath, startTime, endTime);
 
         if (tableNames == null || tableNames.size() < 4) {
             int foundCount = (tableNames == null) ? 0 : tableNames.size();
@@ -144,35 +138,26 @@ public class RocksDBConnector {
             System.out.printf("       - Table %d: %s%n", i + 1, new String(tableNames.get(i)));
         }
 
-        // Test 2: getTableCreationTime
+        // Test 2: getTableIterator
         byte[] testTableNameBytes = tableNames.get(3);
         String testTableNameStr = new String(testTableNameBytes);
-        System.out.printf("%n[TEST] Running: getTableCreationTime%n");
-        System.out.printf("[INFO] Checking creation time for table: %s%n", testTableNameStr);
 
-        long creationTime = TBaseTools.getTableCreationTime(cfHandle, testTableNameStr);
-        System.out.printf("[INFO] Reported creation time: %d%n", creationTime);
-
-        if (creationTime >= startTime && creationTime <= endTime) {
-            System.out.println("[PASS] getTableCreationTime: Creation time is within the expected range.");
-        } else {
-            System.out.printf("[FAIL] getTableCreationTime: Creation time %d is outside the expected range [%d, %d].%n", creationTime, startTime, endTime);
-        }
-
-        // Test 3: getTableIterator
         System.out.printf("%n[TEST] Running: getTableIterator%n");
         System.out.printf("[INFO] Creating iterator for table: %s%n", testTableNameStr);
-        RocksIterator iterator = null;
+        TBaseTableIterator iterator = null;
+
         try {
-            iterator = TBaseTools.getTableIterator(db, cfHandle, testTableNameStr);
+            iterator = TBaseTools.getTableIterator(testTableNameStr);
+            //TBaseTools.debugInfo(iterator);
             if (iterator == null) {
                 System.out.println("[FAIL] getTableIterator: Iterator is null.");
                 return;
             }
-
+            //TBaseTools.debugInfo(iterator);
             int entryCount = 0;
             System.out.println("[INFO] Iterating through key-value pairs:");
             for (iterator.seekToFirst(); iterator.isValid(); iterator.next()) {
+                //TBaseTools.debugInfo(iterator);
                 System.out.printf("       - Key: %-10s | Value: %s%n", new String(iterator.key()), new String(iterator.value()));
                 entryCount++;
             }
@@ -182,6 +167,9 @@ public class RocksDBConnector {
             } else {
                 System.out.println("[WARN] getTableIterator: Iterator is valid, but no entries were found in the table.");
             }
+        } catch (RuntimeException e) {
+            System.out.println("[FAIL] getTableIterator: Iterator is null.");
+            return;
         } finally {
             if (iterator != null) {
                 iterator.close();
@@ -193,24 +181,23 @@ public class RocksDBConnector {
         System.out.println(separator);
     }
 
-    private static void initializeRocksDb() throws RocksDBException,UnsupportedEncodingException{
-            RocksDB.loadLibrary();
+    private static void initializeRocksDb() throws RocksDBException, UnsupportedEncodingException {
+        RocksDB.loadLibrary();
 
-            final String hdfsUri = "hdfs://hostname:port";
+        //final String hdfsUri = "hdfs://hostname:port";
+        //final HdfsEnv hdfsEnv = new HdfsEnv(hdfsUri);
+        Options options = new Options().setCreateIfMissing(true);
+        //options.setEnv(hdfsEnv);
+        options.setDisableAutoCompactions(true);
+        options.setMergeOperatorName("uint64add");
+        options.setMaxBackgroundFlushes(1);
+        options.setWriteBufferSize(50L);
+        options.setBlobSize(256);
+        options.setCreateMissingColumnFamilies(true);
 
-            final HdfsEnv hdfsEnv = new HdfsEnv(hdfsUri);
-            
-            Options options = new Options().setCreateIfMissing(true);
-            options.setEnv(hdfsEnv);
-            options.setDisableAutoCompactions(true);
-            options.setMergeOperatorName("uint64add");
-            options.setMaxBackgroundFlushes(1);
-            options.setWriteBufferSize(50L);
-            options.setBlobSize(256);
-            options.setCreateMissingColumnFamilies(true);
-
-            if (db == null) {
-                db = RocksDB.open( options, hdfsUri + "/username/tmp/testdata");
-            }
+        if (db == null) {
+            //db = RocksDB.open( options, hdfsUri + "/username/tmp/testdata");
+            db = RocksDB.open(options, "/tmp/testdata");
+        }
     }
 }
